b3934cd957b7721e75e457ccfd9237af4efa8296
sivous <540317156@qq.com>
2020-02-13 23:11:31 +0800

    add: search web, README; fix: spider bugs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # baiduimg_spider
 spider for baiduimg
+
+仅作学习之用  
+
+使用前确保已安装flask及其他库，安装完成后运行spider.py服务，然后打开index.html即可  
+一些图片404，莫得办法
--- spider.py
+++ spider.py
@@ -4,46 +4,52 @@
 
 import requests
 import re
+from flask import Flask, request
+from flask_cors import CORS
+import json
 
-method = "GET"
-url = "https://image.baidu.com/search/acjson"
-
-"""
-rn: pics to load
-pn: pics loaded
-queryWord, word: pics keywords
-"""
-keywords = "赛博朋克"
-payload = {
-    "tn": "resultjson_com",
-    "ipn": "rj",
-    "ct": "201326592",
-    "fp": "result",
-    "queryWord": keywords,
-    "cl": "2",
-    "lm": "-1",
-    "ie": "utf-8",
-    "oe": "utf-8",
-    "st": "-1",
-    "ic": "0",
-    "word": keywords,
-    "face": "0",
-    "istype": "2",
-    "nc": "1",
-    "pn": "0",
-    "rn": "30"
-        }
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36",
-    "referer": "https://image.baidu.com"
-}
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
-def spider(method, url, payload, headers):
+
+
+def spider(keywords):
     """
     XHR request
     """
+    method = "GET"
+    url = "https://image.baidu.com/search/acjson"
+    """
+    rn: pics to load
+    pn: pics loaded
+    queryWord, word: pics keywords
+    """
+    keywords = keywords
+    payload = {
+        "tn": "resultjson_com",
+        "ipn": "rj",
+        "ct": "201326592",
+        "fp": "result",
+        "queryWord": keywords,
+        "cl": "2",
+        "lm": "-1",
+        "ie": "utf-8",
+        "oe": "utf-8",
+        "st": "-1",
+        "ic": "0",
+        "word": keywords,
+        "face": "0",
+        "istype": "2",
+        "nc": "1",
+        "pn": "0",
+        "rn": "30"
+            }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36",
+        "referer": "https://image.baidu.com"
+    }
+
     response = requests.request(method, url, params=payload, headers=headers)
     return response
 
@@ -66,7 +72,7 @@ def decode_url(url):
     return re.sub(r'(?P<matched>[0-9a-w])', lambda matched: token.get(matched.group("matched")), url)
 
 
-def parser(pic_info):
+def pic_urls_parser(pic_info):
     """
     parse pics info, extract urls
     """
@@ -77,33 +83,52 @@ def parser(pic_info):
     return pic_urls
 
 
+@app.route('/', methods=["GET", "POST"])
+def exec():
+    """
+    run spider
+    """
+    keywords = request.args.get("search_terms")
+    text = spider(keywords).text
+    pic_urls = pic_urls_parser(text)
+
+    # convert list to json and response
+    reponse = {"urls": pic_urls}
+    return json.dumps(reponse)
+
+    # get name of callback func
+    # callback = request.args.get('callback')
+    # return "callback" + "(" + json.dumps(reponse) + ")"
+
+
 if __name__ == '__main__':
-    text = spider(method, url, payload, headers).text
-    pic_urls = parser(text)
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
     # download pics
-    num = 0
-    for pic_url in pic_urls:
-        cate = pic_url.split(".")[-1]
+    # num = 0
+    # for pic_url in pic_urls:
+    #     cate = pic_url.split(".")[-1]
 
-        content = None
-        try:
-            content = requests.get(url=pic_url, timeout=5).content
-        except requests.exceptions.ConnectTimeout:
-            print("failed to connect", pic_url)
-            continue
-        except requests.exceptions.ReadTimeout:
-            print("failed to read", pic_url)
-            continue
+        # download pics
+        # content = None
+        # try:
+        #     content = requests.get(url=pic_url, timeout=5).content
+        # except requests.exceptions.ConnectTimeout:
+        #     print("failed to connect", pic_url)
+        #     continue
+        # except requests.exceptions.ReadTimeout:
+        #     print("failed to read", pic_url)
+        #     continue
 
-        try:
-            with open("{num}.{cate}".format(num=num, cate=cate), "wb+") as f:
-                f.write(content)
-        except FileNotFoundError:
-            with open("{num}.png".format(num=num), "wb+") as f:
-                f.write(content)
-        except OSError:
-            with open("{num}.png".format(num=num), "wb+") as f:
-                f.write(content)
+        # write down the pics
+        # try:
+        #     with open("{num}.{cate}".format(num=num, cate=cate), "wb+") as f:
+        #         f.write(content)
+        # except FileNotFoundError:
+        #     with open("{num}.png".format(num=num), "wb+") as f:
+        #         f.write(content)
+        # except OSError:
+        #     with open("{num}.png".format(num=num), "wb+") as f:
+        #         f.write(content)
 
-        num += 1
+        # num += 1
